a9b43be73a982798fd03e1290048fae89cbcae75
Matej <mb7628@student.uni-lj.si>
2018-05-30 14:47:12 +0200

    Delo na strežniku

--- streznik/src/app.ts
+++ streznik/src/app.ts
@@ -1,13 +1,17 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { Routes } from "./routes/crmRoutes";
+import "reflect-metadata";
 
 class App {
 
     public app: express.Application;
+    public routePrv: Routes = new Routes();
 
     constructor() {
         this.app = express();
         this.config();
+        this.routePrv.routes(this.app);
     }
 
     private config(): void{
