74fb73926d07f4a1e780089b06836b3c022ee219
matej <mb7628@student.uni-lj.si>
2018-06-24 15:25:58 +0200

    app.ts cleanup

--- streznik/src/app.ts
+++ streznik/src/app.ts
@@ -13,47 +13,18 @@ createConnection().then(async connection => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
 
-    // cors allow
+    // cors rules
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        // res.header("Access-Control-Allow-Credentials", "true");
-        // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        // res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
         next();
     });
 
+    // register routes
     app.use('/api/auth', authRouter);
-
-    // // register express routes from defined application routes
-    // Routes.forEach(route => {
-    //     (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-    //         const result = (new (route.controller as any))[route.action](req, res, next);
-    //         if (result instanceof Promise) {
-    //             result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-    //
-    //         } else if (result !== null && result !== undefined) {
-    //             res.json(result);
-    //         }
-    //     });
-    // });
-
-    // setup express app here
 
     // start express server
     app.listen(3000);
-
-    // // insert new users for test
-    // await connection.manager.save(connection.manager.create(User, {
-    //     firstName: "Timber",
-    //     lastName: "Saw",
-    //     age: 27
-    // }));
-    // await connection.manager.save(connection.manager.create(User, {
-    //     firstName: "Phantom",
-    //     lastName: "Assassin",
-    //     age: 24
-    // }));
 
     console.log("Express server has started on port 3000.");
 
