a1d656152e111747e92867f3148ede3f9b4db31f
AndrejGo <32219868+AndrejGo@users.noreply.github.com>
2018-07-08 20:59:35 +0200

    Osnovna funkcionalnost nastavitev projektov na odjemalcu

--- klient/src/app/app.module.ts
+++ klient/src/app/app.module.ts
@@ -17,10 +17,6 @@ import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import {ProjektComponent} from "./controllers/projekt/projekt.component";
 import {OznacevanjeComponent} from "./controllers/oznacevanje/oznacevanje.component";
 import {NastavitveComponent} from "./controllers/nastavitve/nastavitve.component";
-<<<<<<< HEAD
-import { CollapseModule } from "ngx-bootstrap/collapse";
-=======
->>>>>>> c87b054ccf20367734f9e7061a1045557000ecc0
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -52,7 +48,6 @@ export function HttpLoaderFactory(http: HttpClient) {
                 deps: [HttpClient]
             }
         }),
-        CollapseModule,
         // services
         ServicesModule,
         IndexModule,
