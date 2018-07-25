2a64159b275214f1c31899d54153cab71e5bb85f
Matej <mb7628@student.uni-lj.si>
2018-07-25 16:54:04 +0200

    fix?

--- klient/src/app/app.module.ts
+++ klient/src/app/app.module.ts
@@ -17,9 +17,8 @@ import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import {ProjektComponent} from "./controllers/projekt/projekt.component";
 import {OznacevanjeComponent} from "./controllers/oznacevanje/oznacevanje.component";
 import {NastavitveComponent} from "./controllers/nastavitve/nastavitve.component";
-import {ModalModule, BsModalService} from "ngx-bootstrap";
-import {PaginationModule} from "ngx-bootstrap";
-import { AddFolderComponent } from './controllers/nastavitve/add-folder/add-folder.component';
+import {BsModalService, ModalModule, PaginationModule} from "ngx-bootstrap";
+import {AddFolderComponent} from "./controllers/nastavitve/add-folder/add-folder.component";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
